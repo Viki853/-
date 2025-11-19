2763ae5ab8a62f558cf9dfb3cdbeb9be926836e0
Viki853 <viktoriaalexeenko08@gmail.com>
2025-11-19 12:30:30 +0300

    Add files via upload

---
+++ задача3.py
@@ -0,0 +1,7 @@
+int(input("Введите порядковый номер месяца: "))
+if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+    print("31")
+elif month==2 or month==4 or month==6 or month==9 or month==11:
+    print("30")
+else:
+    print("28")
